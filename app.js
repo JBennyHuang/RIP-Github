d0c9822f872affe2900455ec070c6d3e8bf46627
Shibee <33032568+Shibee@users.noreply.github.com>
2018-10-14 09:21:37 -0400

    commit-Shibe

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var express = require("express");
+var express = require('express');
+
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var receive = require('./io');
 
 app.use(express.static("public"));
-
-app.listen(process.env.PORT,process.env.IP , function(){
-    console.log("Server has started !");
-});
+app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
 
 app.get("/", function(req,res){
-    res.render("home.ejs");
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+receive.receiver(io);
+receive.sender(io);
+
+http.listen(3000 , function(){
+    console.log("Server has started!");
 });
 
